a59dd03b155c155e9adc6b33c39f984d7809c8f0
W. Dev Endd <endercitihacker11111@gmail.com>
2024-12-22 19:41:22 +0700

    ;-;

--- endd-api-installer.sh
+++ endd-api-installer.sh
@@ -5,9 +5,11 @@ read -n 1 bin_links
 if [[ $bin_links == "y" || $bin_links == "Y" ]]; then
   npm install https://github.com/Wd-Endd/endd-api.git;
   cd node_modules/endd-api
+  npm install --only=dev
   tsc
 else
   npm install https://github.com/Wd-Endd/endd-api.git --no-bin-links;
   cd node_modules/endd-api
+  npm install --only=dev --no-bin-links
   tsc
 fi
