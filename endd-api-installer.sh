3b2f067c057355fb224c70e08a2029cf7e39e64e
W. Dev Endd <endercitihacker11111@gmail.com>
2024-12-22 19:34:33 +0700

    ;-;

--- endd-api-installer.sh
+++ endd-api-installer.sh
@@ -3,11 +3,11 @@
 echo "[endd-api-installer] Does your device support binary links? (y/n)"
 read -n 1 bin_links
 if [[ $bin_links == "y" || $bin_links == "Y" ]]; then
-  if [ ! -d "node_modules/endd-api" ]; then npm install https://github.com/Wd-Endd/endd-api.git; fi
+  npm install https://github.com/Wd-Endd/endd-api.git;
   cd node_modules/endd-api
   tsc
 else
-  if [ ! -d "node_modules/endd-api" ]; then npm install https://github.com/Wd-Endd/endd-api.git --no-bin-links; fi
+  npm install https://github.com/Wd-Endd/endd-api.git --no-bin-links;
   cd node_modules/endd-api
   tsc
 fi
