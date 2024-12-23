63c23e15d8cc5471a4f22e697fc2c64451188796
W. Dev Endd <111259650+Wd-Endd@users.noreply.github.com>
2024-12-23 18:27:51 +0700

    Add files via upload

--- endd-api-installer.sh
+++ endd-api-installer.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# move function 
+safe_move() {
+  local source_dir=$1
+  local target_dir=$2
+
+  for file in "$source_dir"/*; do
+    if [ -f "$file" ]; then
+      local base_file=$(basename "$file")
+      if [ -f "$target_dir/$base_file" ]; then
+        cmp -s "$file" "$target_dir/$base_file" || mv "$file" "$target_dir/"
+      else
+        mv "$file" "$target_dir/"
+      fi
+    fi
+  done
+}
+
 echo "[endd-api-installer] Does your device support binary links? (y/n)"
 read -n 1 bin_links
 if [[ $bin_links == "y" || $bin_links == "Y" ]]; then
@@ -20,8 +37,8 @@ if [ -n "$dev" ]; then
   sleep 0.5
   echo "[endd-api-installer] Setting up dev mode..."
   git clone https://github.com/Wd-Endd/endd-api.git
-  mv ./endd-api/* ./
-  rm ./endd-api
+  safe_move "./endd-api/*" "."
+  rm -rf ./endd-api
 fi
 
 echo "[endd-api-installer] Complete!"
